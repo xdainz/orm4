cb7020f7bea84fa1a35ab96fe83b93d7ccbcffa2
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-05-07 14:30:44 -0400

    inicio repo

---
+++ src/main/java/com/hospital/orm4/Orm4Application.java
@@ -0,0 +1,13 @@
+package com.hospital.orm4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Orm4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Orm4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospital/orm4/Orm4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.orm4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Orm4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
